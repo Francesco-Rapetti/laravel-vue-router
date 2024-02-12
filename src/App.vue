e9be500a89fbfe5772549358954ecf9d19d88bd3
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-12 23:32:52 +0100

    Feat: implement tag api call and various layout changes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script>
-import AppComponent from "./components/AppComponent.vue";
+import EventList from "./components/EventList.vue";
+import AppHeader from "./components/AppHeader.vue";
 
 import axios from 'axios'; //importo Axios
 import { store } from "./store.js" //state management
 
 export default {
 	components: {
-		AppComponent
+		EventList,
+		AppHeader
 	},
 	data() {
 		return {
@@ -17,13 +19,24 @@ export default {
 
 		axios.get(this.store.apiURL + "events").then(risultato => {
 			if (risultato.status == 200 && risultato.data.success) {
-				console.log(risultato.data.results);
+				// console.log(risultato.data.results);
 				this.store.events = risultato.data.results;
-				console.log(this.store.events);
+				// console.log(this.store.events);
 			}
 		}).catch(errore => {
 			console.error(errore);
 		});
+
+		axios.get(this.store.apiURL + "tags").then(risultato => {
+			if (risultato.status == 200 && risultato.data.success) {
+				// console.log("tags" + risultato.data.results);
+				this.store.tags = risultato.data.results;
+				// console.log(this.store.tags);
+			}
+		}).catch(errore => {
+			console.error(errore);
+			// console.log("errore")
+		})
 	},
 	methods: {
 
@@ -32,28 +45,9 @@ export default {
 </script>
 
 <template>
+	<AppHeader></AppHeader>
 	<main>
-		<div class="d-flex flex-wrap justify-content-center align-items-center gap-3">
-			<div v-for="event in store.events" class="card h-100" style="width: 18rem;">
-				<div class="card-header">
-					{{ event.id + " - " + event.name }}
-				</div>
-				<ul class="list-group list-group-flush">
-					<li class="list-group-item">DESCRIPTION:</li>
-					<li class="list-group-item">{{ event.description }}</li>
-					<li class="list-group-item">AVAILABLE TICKETS:</li>
-					<li class="list-group-item">{{ event.available_tickets }}</li>
-					<!-- <li class="list-group-item">CREATED AT:</li>
-					<li class="list-group-item">{{ event.created_at }}</li>
-					<li class="list-group-item">UPDATED AT:</li>
-					<li class="list-group-item">{{ event.updated_at }}</li> -->
-					<li v-if="event.tags" class="list-group-item">TAGS:</li>
-					<li v-for="tag in event.tags" class="list-group-item">{{ tag.name }}</li>
-					<li v-if="event.user_id" class="list-group-item">USER:</li>
-					<li v-if="event.user_id" class="list-group-item">{{ event.user.name }}</li>
-				</ul>
-			</div>
-		</div>
+		<router-view></router-view>
 	</main>
 </template>
 
