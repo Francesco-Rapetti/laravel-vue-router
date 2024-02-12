ab81e9a376f6022ef9a0ebbf7ee840e351c23e23
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-12 23:34:26 +0100

    Create event details page

---
+++ src/pages/EventDetails.vue
@@ -0,0 +1,8 @@
+<script></script>
+
+<template>
+    <h1>Event Details Page</h1>
+    <router-link :to="{ name: 'events' }" class="btn btn-primary">Back to events</router-link>
+</template>
+
+<style scoped lang="scss"></style>
