3225b13ce4a3e3cbb7a854d7413dcebcee4a5bf5
Francesco-Rapetti <rapettif80@yahoo.it>
2024-02-13 01:03:43 +0100

    Create Tag list page

---
+++ src/pages/TagList.vue
@@ -0,0 +1,37 @@
+<script>
+import { store } from "../store.js" //state management
+import register from "../debug" //per debuggare il componente da console
+import AppTagCard from "../components/AppTagCard.vue"
+
+export default {
+    name: "AppComponent",
+    components: {
+        AppTagCard
+    },
+    data() {
+        return {
+            store,
+        }
+    },
+
+    mounted() {
+        register(this); //per debuggare il componente da console
+    }
+}
+</script>
+
+<template>
+    <div class="d-flex flex-wrap justify-content-center align-items-center gap-3">
+        <div v-for="tag in this.store.tags">
+            <AppTagCard :name="tag.name" :description="tag.description" :color="tag.color" :id="tag.id">
+            </AppTagCard>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+// importo variabili
+// @use './styles/partials/variables' as *;
+
+// ...qui eventuale SCSS di AppComponent
+</style>
